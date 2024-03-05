bef0b9b7b2dfc87618dc314c8d1de92da8bc1e73
saidulislamjihad <saidulislamjihad20@gmail.com>
2024-03-05 19:30:37 +0600

    home page leptop view done contact us page design start

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,3 +12,23 @@ $('.home__items-wrapper').slick({
   nextArrow:
       '<button type="button" class="slick-next"><i class="fas fa-chevron-right"></i></button>',
 });
+
+// Article Slider js
+$('.article__items-wrapper').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    dots: true,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:3
+        }
+    }
+})
