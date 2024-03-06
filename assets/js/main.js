461ad628430b3854fbe0b99b09efe7e74acd6076
saidulislamjihad <saidulislamjihad20@gmail.com>
2024-03-06 19:35:05 +0600

    fix desig

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,6 +13,26 @@ $('.home__items-wrapper').slick({
       '<button type="button" class="slick-next"><i class="fas fa-chevron-right"></i></button>',
 });
 
+//Count Up Js
+$('.success__item-info-number').each(function() {
+  var $this = $(this),
+    countTo = $this.attr('data-value');
+  $({ countNum: $this.text()}).animate({
+    countNum: countTo
+  },
+  {
+    duration: 10000,
+    easing:'linear',
+    step: function() {
+      $this.text(Math.floor(this.countNum));
+    },
+    complete: function() {
+      $this.text(this.countNum);
+      //alert('finished');
+    }
+  });  
+});
+
 // Article Slider js
 $('.article__items-wrapper').owlCarousel({
     loop:true,
@@ -32,3 +52,46 @@ $('.article__items-wrapper').owlCarousel({
         }
     }
 })
+
+// Testimonial Slider js
+$('.testimonial__slider-items-wrapper').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    dots: true,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
+
+//Dot Js
+$(document).ready(function () {
+    // Function to generate the pattern
+    function generatePattern(rows, columns) {
+      for (var i = rows - 1; i >= 0; i--) {
+        var dotRow = $("<div class='dot-row'></div>");
+
+        for (var j = 0; j < columns; j++) {
+          dotRow.append("<span>&#x2022;</span>");
+        }
+
+        // Set opacity from 0 to 1
+        var opacity = (rows - 1 - i) / (rows - 1); // Adjust this calculation as needed
+        dotRow.css("opacity", opacity);
+
+        $(".contact-info__right-dot-outer").append(dotRow);
+      }
+    }
+
+    // Call the function to generate the pattern
+    generatePattern(10, 10); // Define your rows & columns as parameters
+});
